6ee7ed6fc1e5efac54374ebeedb8a7634cf9a767
abaza <mahmoud.abaza@live.com>
2025-10-14 21:45:10 +0100

    fifth batch

---
+++ Assets/Scripts/CinemachineController.cs
@@ -0,0 +1,19 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CinemachineController : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineCamera _currentTarget;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
